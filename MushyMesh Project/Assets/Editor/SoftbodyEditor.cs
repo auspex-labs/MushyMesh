8c0801a4fe6b129c774af5f0c1d86db5441eb9fb
Ryan Kann <ryanakann@gmail.com>
2019-05-22 18:02:01 -0400

    New softbody script

---
+++ MushyMesh Project/Assets/Editor/SoftbodyEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Softbody))]
+public class SoftbodyEditor : Editor {
+	private bool generated = false;
+
+	public Softbody softbody;
+
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI();
+
+		if (GUILayout.Button("Tense Up")) {
+			((Softbody)target).Tense();
+		}
+
+		if (!generated) {
+			if (GUILayout.Button("Generate Softbody")) {
+				((Softbody)target).GenerateSoftbody();
+			}
+		}
+	}
+}

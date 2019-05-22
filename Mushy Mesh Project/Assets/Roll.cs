e7d50be429b2bcf79e76344d23358e8b43c89f8c
Ryan Kann <ryanakann@gmail.com>
2019-05-22 17:18:12 -0400

    Unity Project Added

---
+++ Mushy Mesh Project/Assets/Roll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxisRaw ("Horizontal");
+        if (input != 0f) {
+            GetComponent<Rigidbody2D> ().AddTorque (30f * Time.deltaTime * input);
+        }   
+    }
+}
